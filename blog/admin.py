cb7dc80632fa326bc2018917e483363e4346d5ea
main <Lucas.neves.ead@gmail.com>
2022-03-08 14:28:41 -0300

    Configuração do admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Post
 
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin) :
+    list_display = ("title", "author", "publicado", "status")
+    list_filter = ("status", "author", "content", "publicado")
+    search_fields = ("title", "content")
+    prepopulated_fields = {"slug": ("title",)}
+    
 # Register your models here.
+"""
+title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE)
+    content = models.TextField()
+    publicado = models.DateTimeField(default=timezone.now)
+    criado = models.DateTimeField(auto_now_add=True)
+    alterado = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10,
+     choices=STATUS,
+    default="rascunho")
+"""
