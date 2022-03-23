9dcba329e445570b015c72775999293df1cd297f
main <Lucas.neves.ead@gmail.com>
2022-03-23 08:49:44 -0300

    Criação do DetailView

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ from . import views
 
 urlpatterns = [
     path('', views.BlogListView.as_view(), name='home'),
+    path('post/<slug:slug>/', views.BlogDetailView.as_view(), name='post_detail'),
 ]
