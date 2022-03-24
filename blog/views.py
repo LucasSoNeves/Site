4c672d4b7e493aa6da92c208be3cc29573a4410e
main <Lucas.neves.ead@gmail.com>
2022-03-24 07:13:38 -0300

    UpdateView

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
+from dataclasses import field
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, UpdateView
 from . models import Post
-
+from django.views.generic.edit import CreateView
 # Create your views here.
 
 class BlogListView(ListView):
@@ -11,3 +12,13 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
+    
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'blog/post_new.html'
+    fields = 'author','title','content'
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'blog/post_edit.html'
+    fields = 'title','content'
